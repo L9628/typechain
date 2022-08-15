082200221e4163a8123d0cfc142169ab76ef9b77
L9628 <mhp4718@gmail.com>
2022-08-15 17:08:03 +0900

    Blocks

--- src/myPackage.js
+++ src/myPackage.js
@@ -1,20 +1,20 @@
-// @ts-check
-/**
- * Initializes the project
- * @param {object} config
- * @param {boolean} config.debug
- * @param {string} config.url
- * @returns boolean
- */
-export function init(config) {
-  return true;
-}
+// // @ts-check
+// /**
+//  * Initializes the project
+//  * @param {object} config
+//  * @param {boolean} config.debug
+//  * @param {string} config.url
+//  * @returns boolean
+//  */
+// export function init(config) {
+//   return true;
+// }
 
-/**
- * Exits the program
- * @param {number} code
- * @returns number
- */
-export function exit(code) {
-  return code + 1;
-}
+// /**
+//  * Exits the program
+//  * @param {number} code
+//  * @returns number
+//  */
+// export function exit(code) {
+//   return code + 1;
+// }
